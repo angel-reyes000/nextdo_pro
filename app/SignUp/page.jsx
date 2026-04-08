858caeb8684313db0a88ade674eafa472ecd5371
Angel Reyes <ar731684@gmail.com>
2026-04-08 02:41:46 -0600

    creacion y diseño de la pagina de sign up

--- app/SignUp/page.jsx
+++ app/SignUp/page.jsx
@@ -1,6 +1,11 @@
 "use client"
 
 import { useState } from 'react';
+import styles from '../styles/signUp.module.scss';
+import cover from '../../public/assets/Sign_in_images/Diseño_portada_login_signup_nextdo_pro_gemini-removebg-preview.png';
+import image_google from '../../public/assets/Sign_in_images/Icono google sin fondo.png';
+import image_apple from '../../public/assets/Sign_in_images/Icono apple sin fondo.png';
+import Image from 'next/image'
 
 export default function SignUp () {
     const [name, setName] = useState('');
@@ -10,7 +15,7 @@ export default function SignUp () {
 
     async function submit (e) {
         e.preventDefault();
-        const res = await fetch(`http://localhost:4000/api/users`, {
+        const res = await fetch('http://localhost:4000/api/users', {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -30,47 +35,61 @@ export default function SignUp () {
 
     return (
         <>
-            <div>
-                <div>
-                    <div>
-                        <div>
+            <div className={styles.window_sign_up}>
+                <div className={styles.container_sign_up}>
+                    <div className={styles.seccions_sign_up}>
+                        <div className={styles.seccion_sign_up_log_in}>
                             <h2>Log in</h2>
                         </div>
-                        <div>
+                        <div className={styles.seccion_sign_up_sign_up}>
                             <h2>Sign up</h2>
                         </div>
                     </div>
-                    <div>
-                        <form onSubmit={(e) => submit(e)}>
-                            <div>
-                                <h1>Join the productivity revolution</h1>
-                                <p>Create your account and start organized today.</p>
-                            </div>
-                            <div>
-                                <div>
-                                    <label htmlFor="name">Name</label>
-                                    <input value={name} onChange={(e) => setName(e.target.value)} name="name" id="name"/>
+                    <div className={styles.form_sign_up}>
+                        <div className={styles.header_sign_up}>
+                            <h1>Join the productivity revolution</h1>
+                            <p>Create your account and start organized today.</p>
+                        </div>
+                        <div className={styles.form_questionary_sign_up}>
+                            <form onSubmit={(e) => submit(e)}>
+                                <div className={styles.inputs_sign_up}>
+                                    <div className={styles.inputs_name_last_name_sign_up}>
+                                        <div>
+                                            <label htmlFor="name">Name</label>
+                                            <input value={name} onChange={(e) => setName(e.target.value)} name="name" id="name"/>
+                                        </div>
+                                        <div>
+                                            <label htmlFor="last_name">Last name</label>
+                                            <input value={lastName} onChange={(e) => setLastName(e.target.value)} name="last_name" id="last_name"/> 
+                                        </div>                                        
+                                    </div>
+                                    <div className={styles.email_password_sign_in}>
+                                        <label htmlFor="email">Email</label>
+                                        <input value={email} onChange={(e) => setEmail(e.target.value)} name="email" id="email" type="email"/>
 
-                                    <label htmlFor="last_name">Last name</label>
-                                    <input value={lastName} onChange={(e) => setLastName(e.target.value)} name="last_name" id="last_name"/>
+                                        <label htmlFor="password">Password</label>
+                                        <input value={password} onChange={(e) => setPassword(e.target.value)} name="password" id="password" type="password"/>
+                                    </div>                                    
                                 </div>
-                                <label htmlFor="email">Email</label>
-                                <input value={email} onChange={(e) => setEmail(e.target.value)} name="email" id="email" type="email"/>
-
-                                <label htmlFor="password">Password</label>
-                                <input value={password} onChange={(e) => setPassword(e.target.value)} name="password" id="password" type="password"/>
-                            </div>
-                            <div>
-                                <button type="submit">Create account</button>
-                                <p>Or continue with </p>
-                            </div>
-                            <div>
-                                <p>imagenes</p>
-                                <p>imagenes</p>
-                            </div>
-                        </form>
-                        <div>
-                            <img />
+                                <div className={styles.button_sign_up}>
+                                    <button type="submit">Create account</button>                                    
+                                </div>
+                                <div className={styles.continue_with_sign_up}>
+                                    <p>Or continue with</p>
+                                    <hr />
+                                </div>
+                                <div className={styles.google_apple_images}>
+                                    <div className={styles.container_image_google}>
+                                        <Image src={image_google} width={70} height={70} alt='Imagen de inicio de sesion con google' />
+                                    </div>
+                                    <div className={styles.container_image_apple}>
+                                        <Image src={image_apple} width={70} height={70} alt='Imagen de inicio de sesion con apple' />
+                                    </div>
+                                </div>
+                            </form>
+                        </div>    
+                        <div className={styles.portada_sign_up}>
+                            <Image src={cover} width={400} height={400} alt='prueba'/>
                         </div>
                     </div>
                 </div>
